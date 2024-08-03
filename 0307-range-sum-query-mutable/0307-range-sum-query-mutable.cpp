162d2aabbfb81c97b0027b05f9d4dc380bd44e03
Vaishnavi <76249972+VaishnaviRashankar@users.noreply.github.com>
2024-08-03 16:37:36 +0530

    Time: 350 ms (85.41%), Space: 177.6 MB (69.3%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -0,0 +1,55 @@
+class FenwickTree {
+ public:
+  FenwickTree(int n) : sums(n + 1) {}
+
+  void add(int i, int delta) {
+    while (i < sums.size()) {
+      sums[i] += delta;
+      i += lowbit(i);
+    }
+  }
+
+  int get(int i) const {
+    int sum = 0;
+    while (i > 0) {
+      sum += sums[i];
+      i -= lowbit(i);
+    }
+    return sum;
+  }
+
+ private:
+  vector<int> sums;
+
+  static inline int lowbit(int i) {
+    return i & -i;
+  }
+};
+
+class NumArray {
+ public:
+  NumArray(vector<int>& nums) : nums(nums), tree(nums.size()) {
+    for (int i = 0; i < nums.size(); ++i)
+      tree.add(i + 1, nums[i]);
+  }
+
+  void update(int index, int val) {
+    tree.add(index + 1, val - nums[index]);
+    nums[index] = val;
+  }
+
+  int sumRange(int left, int right) {
+    return tree.get(right + 1) - tree.get(left);
+  }
+
+ private:
+  vector<int> nums;
+  FenwickTree tree;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
