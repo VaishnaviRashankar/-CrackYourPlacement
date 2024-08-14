44694658d737fb14c69d8407d73a6ca28b667125
Vaishnavi <76249972+VaishnaviRashankar@users.noreply.github.com>
2024-08-14 12:32:27 +0530

    Time: 19 ms (46.06%), Space: 22.8 MB (14.96%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        unordered_map<long, int> cnt;
+        cnt[0] = 1;
+        function<int(TreeNode*, long)> dfs = [&](TreeNode* node, long s) -> int {
+            if (!node) return 0;
+            s += node->val;
+            int ans = cnt[s - targetSum];
+            ++cnt[s];
+            ans += dfs(node->left, s) + dfs(node->right, s);
+            --cnt[s];
+            return ans;
+        };
+        return dfs(root, 0);
+    }
+};
