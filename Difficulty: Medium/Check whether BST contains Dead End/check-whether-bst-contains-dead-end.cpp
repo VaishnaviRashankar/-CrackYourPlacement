9fb1a417ac0df65d57abeccc14ebfacddbbb024e
Vaishnavi <76249972+VaishnaviRashankar@users.noreply.github.com>
2024-08-14 12:28:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check whether BST contains Dead End/check-whether-bst-contains-dead-end.cpp
@@ -0,0 +1,137 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+// } Driver Code Ends
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+
+/*You are required to complete below method */
+
+
+
+
+class Solution {
+public:
+    bool isDeadEnd(Node *root) {
+        return checkDeadEnd(root, 1, INT_MAX);
+    }
+    
+private:
+    bool checkDeadEnd(Node *node, int minVal, int maxVal) {
+        if (!node) return false; // Base case: empty tree
+
+        // If it's a leaf node
+        if (!node->left && !node->right) {
+            // If no other node can be inserted
+            if (minVal == maxVal) return true;
+            return false;
+        }
+
+        // Recur for the left and right subtrees
+        return checkDeadEnd(node->left, minVal, node->data - 1) ||
+               checkDeadEnd(node->right, node->data + 1, maxVal);
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+// bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+     Solution ob;
+     cout<<ob.isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends
