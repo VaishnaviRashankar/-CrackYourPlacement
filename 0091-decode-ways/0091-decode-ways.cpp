b507181057f6df8cec6b2a44f66d7d314aa94271
Vaishnavi <76249972+VaishnaviRashankar@users.noreply.github.com>
2025-05-12 11:49:22 +0530

    Time: 0 ms (100%), Space: 9.2 MB (31.35%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,28 +1,27 @@
 class Solution {
- public:
-  int numDecodings(string s) {
-    const int n = s.length();
-    // dp[i] := the number of ways to decode s[i..n)
-    vector<int> dp(n + 1);
-    dp[n] = 1;  // ""
-    dp[n - 1] = isValid(s[n - 1]);
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+         if (n == 0 || s[0] == '0') return 0;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = s[0] != '0' ? 1 : 0;
 
-    for (int i = n - 2; i >= 0; --i) {
-      if (isValid(s[i]))
-        dp[i] += dp[i + 1];
-      if (isValid(s[i], s[i + 1]))
-        dp[i] += dp[i + 2];
+        for(int i=2;i<=n;i++)
+        {
+        int oneDigit = stoi(s.substr(i - 1, 1));
+        int twoDigit = stoi(s.substr(i - 2, 2));
+
+        if(oneDigit >=1)
+        {
+        dp[i]+=dp[i-1];
+        }
+        if(twoDigit>=10 && twoDigit <=26)
+        {
+        dp[i]+=dp[i-2];
     }
+ }
 
-    return dp[0];
-  }
-
- private:
-  bool isValid(char c) {
-    return c != '0';
-  }
-
-  bool isValid(char c1, char c2) {
-    return c1 == '1' || c1 == '2' && c2 < '7';
-  }
+    return dp[n];
+    }
 };
