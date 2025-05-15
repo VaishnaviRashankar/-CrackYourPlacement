210a42111b4dbacf6659d80adb67062b3f7f28e1
Vaishnavi <76249972+VaishnaviRashankar@users.noreply.github.com>
2025-05-15 11:17:32 +0530

    Time: 26 ms (64.71%), Space: 16.3 MB (76.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,28 @@
 class Solution {
- public:
-  int numIslands(vector<vector<char>>& grid) {
-    constexpr int dirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    const int m = grid.size();
-    const int n = grid[0].size();
-    int ans = 0;
+public:
+    int numIslands(vector<vector<char>>& grid) {
+      int ans=0;
+      for(int i=0;i<grid.size();i++)
+      for(int j=0;j<grid[0].size();j++)
+      if(grid[i][j]=='1')
+      {
+        dfs(grid,i,j);
+        ans++;
+      }  
+      return ans;
+    }
+    private:
+    void dfs(vector<vector<char>>&grid,int i,int j)
+    {
+        if(i<0||i==grid.size()||j<0||j==grid[0].size())
+        return ;
+        if(grid[i][j]!='1')
+        return ;
 
-    auto bfs = [&](int r, int c) {
-      queue<pair<int, int>> q{{{r, c}}};
-      grid[r][c] = '2';  // Mark '2' as visited.
-      while (!q.empty()) {
-        const auto [i, j] = q.front();
-        q.pop();
-        for (const auto& [dx, dy] : dirs) {
-          const int x = i + dx;
-          const int y = j + dy;
-          if (x < 0 || x == m || y < 0 || y == n)
-            continue;
-          if (grid[x][y] != '1')
-            continue;
-          q.emplace(x, y);
-          grid[x][y] = '2';  // Mark '2' as visited.
-        }
-      }
-    };
-
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        if (grid[i][j] == '1') {
-          bfs(i, j);
-          ++ans;
-        }
-
-    return ans;
-  }
+        grid[i][j]='2'; //mark 2' as visited
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
 };
